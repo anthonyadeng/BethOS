dd3fc3e65f97093c91b4e11954db7ceee9ebc4ff
Anthony Deng <anthonyadeng@gmail.com>
2024-12-02 13:33:15 -0500

    fix styling

--- app/page.tsx
+++ app/page.tsx
@@ -50,48 +50,39 @@ export default function Home() {
       </header>
       <main className='flex flex-col w-full h-screen bg-slate-100 text-zinc-900  overflow-hidden items-center'>
         <div
-          className='blur-2xl opacity-60 rounded-full overflow-hidden '
+          className='blur-2xl absolute opacity-60 w-[111px] sm:w-[222px] h-[111px] sm:h-[222px] rounded-full bottom-3 right-3 overflow-hidden z-0 sm:z-10'
           style={{
-            position: 'absolute',
-            zIndex: 10,
-            bottom: 0,
-            right: 0,
-            width: 222,
-            height: 222,
             background:
               'radial-gradient(closest-side, rgba(147, 128, 201, 0.94), rgba(98, 139, 237, 0.82), transparent)',
           }}
         ></div>
         <div
-          className='blur-2xl opacity-30 rounded-full overflow-hidden '
+          className='absolute blur-2xl opacity-30 w-[111px] sm:w-[222px] h-[111px] sm:h-[222px] bottom-3 right-3 rounded-full overflow-hidden z-0 sm:z-10'
           style={{
-            position: 'absolute',
-            zIndex: 10,
-            bottom: 10,
-            right: 10,
-            width: 222,
-            height: 111,
             background:
               'conic-gradient(rgba(92, 110, 161, 0.8), rgba(233, 38, 50, 0.85), transparent)',
           }}
         ></div>
-        <div className=' z-10 pointer-events-none bottom-0 left-1/2 before:absolute before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[""]  before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-rose-400 before:opacity-50 before:w-48 before:lg:w-96 before:h-screen overflow-hidden'></div>
+        <div className='z-0 sm:z-10 pointer-events-none bottom-0 left-1/2 before:absolute before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[""]  before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-rose-400 before:opacity-50 before:w-48 before:lg:w-96 before:h-screen overflow-hidden'></div>
         <div className='w-full h-screen static justify-items-center items-center place-content-center place-items-center'></div>
         <div className='w-full min-h-screen bg-transparent z-1 overflow-y-scroll overflow-x-hidden flex-row justify-center'>
-          <div className='text-xl sm:text-3xl  right-0 text-left absolute bottom-0 font-sans font-medium py-16 px-12 w-1/2 lg:w-1/3'>
+          <div className='text-2xl sm:text-3xl  right-0 text-left absolute bottom-0 font-sans font-medium py-16 px-12 w-3/4 sm:w-1/2 lg:w-1/3'>
             <p>{'Bethany Hughes (b. 1992, Canada) '}</p>
-            <p className='text-sm sm:text-lg'>
+            <p className='text-base sm:text-lg'>
               {
                 '-> Conservation Assistant at Athena Art Conservation in NYC, focusing on paintings and paper artworks.'
               }
             </p>
-            <div className='relative w-1/2 h-12 p-0 min-w-36'>
+            <div
+              className='relative w-1/2 h-12 p-0 min-w-48 max-w-60 -my-2 z-50 pointer-events-auto'
+              onClick={handleClick}
+            >
               <Image
                 src={e}
                 alt='e'
-                onClick={handleClick}
                 fill
-                className='pointer-events-auto object-contain'
+                sizes='33vw'
+                className=' object-contain object-left '
               />
             </div>
           </div>
